a44cee2c3fed55b2477f344b67a80ba319e49158
Branko Van Tendeloo <r0949489@student.thomasmore.be>
2023-03-07 11:42:45 +0100

    lab5.2 - 1 added

--- labs/lab5.2 - 1.py
+++ labs/lab5.2 - 1.py
@@ -1,24 +1,25 @@
 import time
 import wiringpi
-import sys
-
-def blink(_pin):
-    print("Led Blinks")
-    wiringpi.digitalWrite(_pin, 1)
-    time.sleep(0.5)
-    wiringpi.digitalWrite(_pin, 0)
-    time.sleep(0.5)
 
 
 #SETUP
 print("Start blinking")
-pin = 1
+switch = 2
+led = 1
 wiringpi.wiringPiSetup()
-wiringpi.pinMode(pin, 1)
+wiringpi.pinMode(led, 1)
+wiringpi.pinMode(switch, 0)
 
 
 #MAIN
 while True:
-    if wiringpi.digitalRead(pin) == 0:
-        blink(pin)
+    if wiringpi.digitalRead(switch) == 0:
+        print("LED Blinks")
+        wiringpi.digitalWrite(led, 1)
+        time.sleep(0.5)
+        wiringpi.digitalWrite(led, 0)
+        time.sleep(0.5)
 
+    else:
+        print("LED is not Flashing")
+        time.sleep(1)
