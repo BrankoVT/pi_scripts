b75dba7bf3d8c0dd947f626d3c455dfd2d68a811
Branko Van Tendeloo <r0949489@student.thomasmore.be>
2023-03-28 11:04:06 +0200

    lab6 - 3

---
+++ labs/lab6 - 3.py
@@ -0,0 +1,38 @@
+import wiringpi
+import time
+
+def ActivateLM35 ():
+    wiringpi.digitalWrite(pin_CS_adc, 0)       # Actived ADC using CS
+    time.sleep(0.000005)
+
+def DeactivateLM35():
+    wiringpi.digitalWrite(pin_CS_adc, 1)       # Deactived ADC using CS
+    time.sleep(0.000005)
+
+def readLM35(adcnum): 
+    if ((adcnum > 7) or (adcnum < 0)): 
+        return -1 
+    revlen, recvData = wiringpi.wiringPiSPIDataRW(1, bytes([1,(8+adcnum)<<4,0]))
+    time.sleep(0.000005)
+    adcout = ((recvData[1]&3) << 8) + recvData[2] 
+    
+    return adcout  
+
+#Setup
+pin_CS_adc = 16                                 #We will use w16 as CE, not the default pin w15!
+wiringpi.wiringPiSetup() 
+wiringpi.pinMode(pin_CS_adc, 1)                 # Set ce to mode 1 ( OUTPUT )
+wiringpi.wiringPiSPISetupMode(1, 0, 500000, 0)  #(channel, port, speed, mode)
+
+#Main
+try:
+    while True:
+        ActivateLM35()
+        tmp0 = readLM35(0) # read channel 0
+        DeactivateLM35()
+        print("temperature:", round(tmp0*100*3.3/1023, 3), "°C")
+        time.sleep(0.2)
+
+except KeyboardInterrupt:
+    DeactivateLM35()
+    print("\nProgram terminated")
